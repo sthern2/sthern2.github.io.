9b95f73f4d333a98b73477e04f043e2bc9c127f1
Sthefany <smnunes99@gmail.com>
2024-06-15 16:02:24 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let card = document.getElementById("card");
+
+// Exibir a mensagem caso seja selecionado o 'sim'
+document.querySelector("#yes").addEventListener("click", function () {
+  let question = document.getElementById("question");
+  let message = document.getElementById("answer");
+
+  
+  question.style.display = "none";
+  message.style.display = "flex";
+});
+
+// Movimento aleatório do botão 'não'
+document.querySelector("#no").addEventListener("mouseover", function () {
+  let width = window.innerWidth - 50;
+  let height = window.innerHeight - 50;
+
+  this.style.position = "absolute";
+  this.style.top = Math.random() * width + "px";
+  this.style.left = Math.random() * height + "px";
+});
